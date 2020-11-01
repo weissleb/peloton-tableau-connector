c3c24164ceb82ce0353fc9573393df8b0ee33097
Brian Weissler <weissleb@gmail.com>
2020-11-01 09:33:25 -0500

    Basics are working in Tableau.

--- pelotonWDC.js
+++ pelotonWDC.js
@@ -3,28 +3,20 @@
 
     myConnector.getSchema = function (schemaCallback) {
         let typeConversion = new Map();
-        typeConversion.set("string", "tableau.dataTypeEnum.string");
-        typeConversion.set("int32", "tableau.dataTypeEnum.int");
-        typeConversion.set("uint16", "tableau.dataType.int");
-        typeConversion.set("uint64", "tableau.dataType.int");
-        typeConversion.set("float32", "tableau.dataTypeEnum.float");
-        typeConversion.set("bool", "tableau.dataTypeEnum.bool");
-        typeConversion.set("time.Time", "tableau.dataTypeEnum.datetime");
+        typeConversion.set("string", tableau.dataTypeEnum.string);
+        typeConversion.set("int32", tableau.dataTypeEnum.int);
+        typeConversion.set("uint16", tableau.dataTypeEnum.int);
+        typeConversion.set("uint64", tableau.dataTypeEnum.int);
+        typeConversion.set("float32", tableau.dataTypeEnum.float);
+        typeConversion.set("bool", tableau.dataTypeEnum.bool);
+        typeConversion.set("time.Time", tableau.dataTypeEnum.datetime);
 
         var tableSchemas = [];
 
-        $.ajax({
-            // beforeSend: function (request) {
-            //     request.setRequestHeader("Access-Control-Allow-Origin", "*");
-            // },
-            dataType: "json",
-            url: "http://localhost:30000/cycling/schema",
-        }).done(function (data) {
-            var tables = data.tables
+        $.getJSON("http://localhost:30000/cycling/schema?tables=workouts", function(resp) {
+            var tables = resp.tables
             for (var t = 0, tlen = tables.length; t < tlen; t++) {
-                /*
-                "name", "description", "columns"
-                */
+                // "name", "description", "columns"
                 var cols = []
                 table = tables[t]
                 columns = table.columns
@@ -51,29 +43,28 @@
     };
 
     myConnector.getData = function (table, doneCallback) {
-        if (table.tableInfo.id == "workouts") {
-            $.ajax({
-                dataType: "json",
-                url: "http://localhost:30000/cycling/data",
-            }).done(function (data) {
-                table.appendRows(data.data);
-            });
-        }
+        $.getJSON("http://localhost:30000/cycling/data", function(resp) {
+            var tableData = [];
 
-        if (table.tableInfo.id == "dummy") {
-            tableData = []
-            row = {
-                "stringField": "foo", 
-                "intField": 99,
-                "boolField": true,
-                "floatField": 99.9,
-                "dateField": "1976-01-07 11:05:30"
-            };
-            tableData.push(row)
+            if (table.tableInfo.id == "workouts") {
+                tableData = resp.data;
+                tableau.log("This is your Peloton data with " + tableData.length + " rows.");
+            }
+
+            if (table.tableInfo.id == "dummy") {
+                row = {
+                    "stringField": "foo",
+                    "intField": 99,
+                    "boolField": true,
+                    "floatField": 99.9,
+                    "dateField": "1976-01-07 11:05:30"
+                };
+                tableData.push(row)
+            }
+
             table.appendRows(tableData)
-        }
-        tableau.log("This is your Peloton data.");
-        doneCallback();
+            doneCallback();
+        });
     };
 
     tableau.registerConnector(myConnector);
@@ -85,3 +76,7 @@
         });
     });
 })();
+
+function f() {
+    console.log("Fuck you!")
+}
